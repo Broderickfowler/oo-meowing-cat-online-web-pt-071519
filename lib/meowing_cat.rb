af4d8b5f4d657616db70b1cd149d779a36db608b
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-18 01:38:04 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -8,4 +8,22 @@ class Cat
   end
 end
 
+
+
+
+
+
+class Dog
+  def initialize(breed)
+    @breed = breed
+  end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
+end
   
